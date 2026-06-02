87db8ed7f436d36fa7739f22a9c353b01dfd7558
Lakshy Aggarwal <lakshyaggarwal977@gmail.com>
2026-06-02 15:20:35 +0530

    ninth cmmit

---
+++ CSE-13 REACT/my-react-app/app.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        <iframe src="https://www.youtube.com/embed/OQthUZMgiS0" allowFullScreen></iframe>
+   <p>Welcome to my webpages of abesec</p>
+   <p>Hello</p>
+   <h2>CSE 13</h2>
+   <img src="https://images.pexels.com/photos/30002390/pexels-photo-30002390.jpeg" width="500" height="500"></img>
+   
+   </div>
+  )
+}
+
+export default App
